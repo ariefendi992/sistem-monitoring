023360424aab89f82f0fb8c991afdaf75602b4d3
Ari Efendi <ariefendi992@gmail.com>
2023-08-29 04:38:20 +0800

    update loadenv

--- settings.py
+++ settings.py
@@ -2,7 +2,10 @@ from datetime import timedelta
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
